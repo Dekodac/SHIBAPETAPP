2d2e3b08113ec84332c9df4fc0b30d9d33b5e2fe
Dakota Cannon <Dekodac@gmail.com>
2023-08-10 18:05:08 -0400

    Registered apps on admin site, finished models for now.. , created fixtures and preset shiba characters in db.

--- shiba_app/models.py
+++ shiba_app/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from users_app.models import User
 
-class Shiba():
-    name = models.CharField()
+class Shiba(models.Model):
+    name = models.CharField(max_length=100)
+    eyecolor = models.CharField(max_length=8)
+    furcolor = models.CharField(max_length=8)
+    accessories = models.CharField(max_length=100)
+    personality = models.TextField()
+    experience_points = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+    
--- users_app/models.py
+++ users_app/models.py
@@ -3,8 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField(verbose_name='email address', max_length=255,unique=True)
-    username = models.CharField(max_length=15)
+
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    def __str__(self) -> str:
+        return self.username
 
