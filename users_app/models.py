a6234342c2fed61ecfd14b9de0883504194ee334
Dakota Cannon <Dekodac@gmail.com>
2023-08-12 14:05:53 -0400

    fixed user_app model required fields

--- users_app/models.py
+++ users_app/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    email = models.EmailField(verbose_name='email address', max_length=255,unique=True)
+    email = models.EmailField( max_length=255,unique=True)
 
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self) -> str:
         return self.username
